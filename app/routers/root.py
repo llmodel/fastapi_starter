c1d38ff0bfc739337b913947e4b5c64ae145d212
Open Source Developer <llgm759@gmail.com>
2025-02-03 13:15:35 +0900

    injust logger dependency into root endpoint function

--- app/routers/root.py
+++ app/routers/root.py
@@ -1,12 +1,14 @@
 # app/routers/root.py
-from fastapi import APIRouter, Security
+from fastapi import APIRouter, Depends, Security
 from app.dependencies import get_logger
 from app.utils.api_key import get_api_key
-
-logger = get_logger()
 
 router = APIRouter()
 
 @router.get("/api/v1")
-async def root(api_key: str = Security(get_api_key)):
-    return {"message": "Welcome to the FastAPI starter app!"}
+async def root(
+  api_key: str = Security(get_api_key),
+  logger=Depends(get_logger)
+):
+  logger.info("Root endpoint accessed")
+  return {"message": "Welcome to the FastAPI starter app!"}
